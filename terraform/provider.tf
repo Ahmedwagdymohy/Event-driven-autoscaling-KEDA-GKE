45fb9766e6bca1235516f6bf33102f5412297e08
Ahmed-Wagdy <ahmed.wagdy@incorta.com>
2025-11-17 01:02:31 +0200

    Adding the provider as google

---
+++ terraform/provider.tf
@@ -0,0 +1,18 @@
+
+
+
+provider "google" {
+  project     = "supple-alpha-474315-q5"
+  region      = "us-east1"
+}
+
+
+
+# defining the backend file to store the terraform state
+terraform {
+  backend "gcs" {
+    bucket = "tf-state-keda"
+    prefix = "terraform/state"
+    
+  }
+}
