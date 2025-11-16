b54c5d97a656289a00b231995e361e14c0d5e2f7
Ahmed-Wagdy <ahmed.wagdy@incorta.com>
2025-11-17 01:02:52 +0200

    Creating the main VPC network of the cluster

---
+++ terraform/vpc.tf
@@ -0,0 +1,25 @@
+# we should enable the compute and container APIs for GKE clusters
+resource "google_project_service" "compute" {
+  service = "compute.googleapis.com"
+}
+
+
+resource "google_project_service" "container" {
+  service = "container.googleapis.com"
+}
+
+
+resource "google_compute_network" "main" {
+  name                    = "main-vpc-network"
+  auto_create_subnetworks = false
+  routing_mode = false
+  mtu = 1460
+  delete_default_routes_on_create = false
+
+    # this tells terraform to wait until the APIs are enabled
+    depends_on = [
+        google_project_service.compute,
+        google_project_service.container,
+    ]
+
+}
