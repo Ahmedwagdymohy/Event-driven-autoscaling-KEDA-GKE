d03f5f25bfe1bc28bc8627614207b00ceab99fa9
Ahmed-Wagdy <ahmed.wagdy@incorta.com>
2025-11-17 09:47:08 +0200

    Adding the firewall to access the instances

---
+++ terraform/firewall.tf
@@ -0,0 +1,15 @@
+# this file is used to open ssh port to access the instances
+resource "google_compute_firewall" "allow-ssh" {
+    name = "allow-ssh"
+    network = google_compute_network.main.name
+
+    allow {
+        protocol = "tcp"
+        ports    = ["22"]
+    }
+
+
+
+    source_ranges = ["0.0.0.0/0"]
+  
+}
